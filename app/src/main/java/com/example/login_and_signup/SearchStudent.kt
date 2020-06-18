6029cc11c6b0f026a7f82bb22820e325b701a091
Sushruth17 <sushruth.sushe@gmail.com>
2020-06-19 00:42:21 +0530

    hides keyboard in search student activity after search

--- app/src/main/java/com/example/login_and_signup/SearchStudent.kt
+++ app/src/main/java/com/example/login_and_signup/SearchStudent.kt
@@ -1,6 +1,10 @@
 package com.example.login_and_signup
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -12,9 +16,18 @@ class SearchStudent : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_student)
 
+        fun Context.hideKeyboard(view: View) {
+            val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+        }
 
         val buttonsearch = findViewById<Button>(R.id.search_name)
         buttonsearch.setOnClickListener {
+
+            fun Activity.hideKeyboard() {
+                hideKeyboard(currentFocus ?: View(this))
+            }
+            hideKeyboard()
             val editName = findViewById(R.id.editName) as EditText
 //        val name: String = editName.getText().toString()
             Toast.makeText(this, editName.text, Toast.LENGTH_SHORT).show()
