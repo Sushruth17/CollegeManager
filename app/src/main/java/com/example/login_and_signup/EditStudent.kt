f4aac735d229392bc89b58409a59d5edfa4bf415
Sushruth17 <sushruth.sushe@gmail.com>
2020-06-23 04:05:34 +0530

    added edit text validation using regex

--- app/src/main/java/com/example/login_and_signup/EditStudent.kt
+++ app/src/main/java/com/example/login_and_signup/EditStudent.kt
@@ -1,85 +1,121 @@
 package com.example.login_and_signup
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Parcelable
+import android.text.Editable
+import android.text.InputFilter
+import android.text.TextWatcher
 import android.util.Log
+import android.view.View
 import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.example.login_and_signup.model.InfoItem
-import com.example.login_and_signup.model.StudentInfoModel
 import com.example.login_and_signup.utils.ApiAddStudent
 import com.example.login_and_signup.utils.StringUtils
 import com.google.gson.JsonObject
-import kotlinx.android.synthetic.main.activity_add_student.*
 import kotlinx.android.synthetic.main.activity_edit_student.*
-import kotlinx.android.synthetic.main.activity_edit_student.view.*
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+
 
 class EditStudent : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_student)
 
+
         val dataReceived =intent.getParcelableExtra<InfoItem>(StringUtils.STUDENT_EDIT_DATA)
         Log.i("Data","----Data received-----"+ dataReceived)
 
+        var age = "0"
+        if (dataReceived.age != null )
+            age = dataReceived.age.toString()
 
         edit_name.setText(dataReceived.name)
-//        edit_age.setText(dataReceived.age)
+        edit_age.setText(age)
         edit_address.setText(dataReceived.address)
         edit_parent_name.setText(dataReceived.parentname)
 
 
+        edit_name.filters = validateET("[A-Za-z ]+")
+        edit_parent_name.filters = validateET("([A-Za-z ])+")
+
         val buttonEdit = findViewById<Button>(R.id.btn_edit)
         buttonEdit.setOnClickListener {
-
-            val name_edited: String = edit_name.text.toString()
-            Log.i("add", "--------ADDNAME-------------- " + name_edited)
-            val address_edited: String = edit_address.text.toString()
-            Log.i("add", "--------ADDADDRES-------------- " + address_edited)
-            val age_edited: String = edit_age.text.toString()
-            Log.i("add", "--------ADDAGE-------------- " + age_edited)
-            val parentname_edited: String = edit_parent_name.text.toString()
-            Log.i("add", "--------ADDPARENTNAME-------------- " + parentname_edited)
-            val idOfEdited = dataReceived.id
-
-
-            val jsonEditObj = JsonObject()
-            jsonEditObj.addProperty("name", name_edited)
-            jsonEditObj.addProperty("address", address_edited)
-            jsonEditObj.addProperty("age", age_edited)
-            jsonEditObj.addProperty("parentname", parentname_edited)
-            jsonEditObj.addProperty("id", idOfEdited)
-            //  POST demo
-            ApiAddStudent()
-                .addRetroFit()
-                ?.editStudent(jsonEditObj)
-                ?.enqueue(object : Callback<ResponseBody> {
-                    override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                        println("---TTTT :: POST Throwable EXCEPTION:: " + t.message)
-                    }
-
-                    override fun onResponse(
-                        call: Call<ResponseBody>,
-                        response: Response<ResponseBody>
-                    ) {
-                        if (response.isSuccessful) {
-                            val msg = response.body()?.string()
-                            println("---TTTT :: POST msg from server :: " + msg)
-                            Toast.makeText(applicationContext, msg, Toast.LENGTH_SHORT).show()
-                        }
-                    }
-                })
-            val intent = Intent(this, Home::class.java)
-            startActivity(intent)
+            doThis(dataReceived);
         }
     }
+
+    private fun doThis(dataReceived: InfoItem?) {
+
+
+//            val name_edited: String = edit_name.text.toString()
+        val nameEdited = edit_name.text.toString()
+        Log.i("add", "--------ADDNAME-------------- " + nameEdited)
+        val address_edited: String = edit_address.text.toString()
+        Log.i("add", "--------ADDADDRES-------------- " + address_edited)
+        val age_edited: String = edit_age.text.toString()
+        Log.i("add", "--------ADDAGE-------------- " + age_edited)
+        val parentname_edited: String = edit_parent_name.text.toString()
+        Log.i("add", "--------ADDPARENTNAME-------------- " + parentname_edited)
+        val idOfEdited = dataReceived?.id
+
+
+        val jsonEditObj = JsonObject()
+        jsonEditObj.addProperty("name", nameEdited)
+        jsonEditObj.addProperty("address", address_edited)
+        jsonEditObj.addProperty("age", age_edited)
+        jsonEditObj.addProperty("parentname", parentname_edited)
+        jsonEditObj.addProperty("id", idOfEdited)
+        //  POST demo
+        ApiAddStudent()
+            .addRetroFit()
+            ?.editStudent(jsonEditObj)
+            ?.enqueue(object : Callback<ResponseBody> {
+                override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                    println("---TTTT :: POST Throwable EXCEPTION:: " + t.message)
+                }
+
+                override fun onResponse(
+                    call: Call<ResponseBody>,
+                    response: Response<ResponseBody>
+                ) {
+                    if (response.isSuccessful) {
+                        val msg = response.body()?.string()
+                        println("---TTTT :: POST msg from server :: " + msg)
+                        Toast.makeText(applicationContext, msg, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            })
+        val intent = Intent(this, Home::class.java)
+        startActivity(intent)
+
+
+    }
+
+    private fun validateET(regex: String): Array<InputFilter>? {
+        return arrayOf(
+            InputFilter { src, start, end, dst, dstart, dend ->
+                if (src == "") { // for backspace
+                    Log.i("regex","---------REGEX_ if backspace--------")
+                    return@InputFilter src
+
+                }
+                if (StringUtils.checkRegex(src,regex)){
+                    Log.i("regex","---------REGEX_ if given expression--------")
+                        src
+                    } else ""
+
+            }
+        )
+
+    }
+
 }
+
 
 
--- app/src/main/java/com/example/login_and_signup/utils/StringUtils.kt
+++ app/src/main/java/com/example/login_and_signup/utils/StringUtils.kt
@@ -1,7 +1,19 @@
 package com.example.login_and_signup.utils
 
+import android.util.Log
+
 class StringUtils {
     companion object{
+
+        fun checkRegex(src: CharSequence?, regex: String): Boolean {
+            Log.i("regex","---------REGEX_ inside checkRefex--------")
+            if (src.toString().matches(regex.toRegex())) {
+                Log.i("regex", "---------REGEX_ if matches--------")
+                return true
+            }
+            return false
+        }
+
         val STUDENT_INFO_DATA = "STUDENT_INFO_DATA"
 
         val STUDENT_SEARCH_DATA = "STUDENT_SEARCH_DATA"
@@ -9,4 +21,5 @@ class StringUtils {
         val STUDENT_EDIT_DATA = "STUDENT_EDIT_DATA"
 
     }
+
 }
