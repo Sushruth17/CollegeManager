3d184b2908e872946d4d4006ad7d2cfe5179faa6
Sushruth17 <sushruth.sushe@gmail.com>
2020-07-30 19:51:32 +0530

    app not installing error

--- app/src/main/java/com/example/login_and_signup/Documents.kt
+++ app/src/main/java/com/example/login_and_signup/Documents.kt
@@ -1,12 +1,14 @@
 package com.example.login_and_signup
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.View
-import kotlinx.android.synthetic.main.activity_create_user.*
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_documents.*
 
 class Documents : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_documents)
@@ -17,4 +19,5 @@ class Documents : AppCompatActivity() {
         })
 
     }
+
 }
